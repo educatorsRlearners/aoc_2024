d80e2f3131dfa6cd604b4678c0638de5ce0510b5
educatorsRlearners <p.evansimpson@gmail.com>
2024-12-18 16:53:15 +0100

    Added source and simplified code

--- Scripts/18.py
+++ Scripts/18.py
@@ -1,32 +1,35 @@
+# Minimally modified from https://github.com/fuglede/adventofcode/blob/master/2024/day18/solutions.py
+
 import networkx as nx
 
 sample_input = "data/18_sample.txt"
 
 input_file = "data/18_data.txt"
 
-
-sample_grid_space = 7
-grid_space = 71
-
-
-def get_grid_space(size_of_gride):
-    return nx.grid_2d_graph(size_of_gride, size_of_gride)
-
-
-def get_max_dist(grid_space):
-    return (grid_space - 1, grid_space - 1)
-
-
 with open(input_file) as f:
     ns = [tuple(map(int, l.split(","))) for l in f.read().strip().split("\n")]
 
-G = get_grid_space(grid_space)
+size_of_grid = max(max(ns))
 
-max_dist = get_max_dist(grid_space)
+
+if size_of_grid == 70:
+    BYTES = 1023
+else:
+    BYTES = 11
+
+
+def get_grid_space(size_of_grid):
+    return nx.grid_2d_graph(size_of_grid + 1, size_of_grid + 1)
+
+
+max_dist = (size_of_grid, size_of_grid)
+
+G = get_grid_space(size_of_grid)
+
 
 for i, p in enumerate(ns):
     G.remove_node(p)
-    if i == 1023:
+    if i == BYTES:
         # Part 1
         print(nx.shortest_path_length(G, (0, 0), max_dist))
     elif not nx.has_path(G, (0, 0), max_dist):
